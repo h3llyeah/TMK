3fbe01fd1df22aeede2b2a2978a90764cc88b925
h3llyeah <hniilo@gmail.com>
2016-05-03 09:02:33 +0300

    Added files via upload

---
+++ ylesanded/yl0204.php
@@ -0,0 +1,8 @@
+https://github.com/h3llyeah/phpkursus2016/tree/master/ylesanded<br>
+
+<?php
+$citizen=array("Albert",64,"treial","Harjumaa",51455435);
+
+echo "Kodanik $citizen[0], kes on $citizen[1] aastat vana, töötas $citizen[3] KEKis ametikohal
+	$citizen[2]. Tema kontakt number on $citizen[4]";
+?>
