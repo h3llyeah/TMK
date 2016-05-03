e4cb89faffd1e05a1bf68b246719b7625b14697d
h3llyeah <hniilo@gmail.com>
2016-05-03 08:52:59 +0300

    Added files via upload

---
+++ ylesanded/yl0203.php
@@ -0,0 +1,12 @@
+https://github.com/h3llyeah/phpkursus2016/tree/master/ylesanded
+
+<?php
+
+$names=array("Kiisu","Miisu","Tiisu","Siisu","Kiuks","Valge","Puma", "Hõbe", "Murdja", "Hellik");
+
+foreach ($names as $a)
+{
+	echo "$a <br>";
+}
+
+?>
