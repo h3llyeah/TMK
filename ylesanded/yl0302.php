5dd8e3a5630594dca54c6fe27e9d8e31db35a49c
h3llyeah <hniilo@gmail.com>
2016-05-13 15:48:21 +0300

    Add files via upload

---
+++ ylesanded/yl0302.php
@@ -0,0 +1,23 @@
+https://github.com/h3llyeah/phpkursus2016/tree/master/ylesanded </br></br>
+<?php
+function print_array($array)
+{
+	for ($i=0;$i<sizeof($array);$i++)
+	{
+		  echo ($i+1).". $array[$i] ";
+	}
+}
+
+function vaheta_elemendid($i,$j, $array)
+{
+	$a=$array[$i];
+	$array[$i]=$array[$j];
+	$array[$j]=$a;
+	return $array;
+}
+
+$mas=['Karu','Jänes','Hunt','Rebane','Hirv','Hiir','Põder','Ahv','Mäger','Mutt'];
+print_array($mas);
+print_array(vaheta_elemendid(0,1,$mas))
+
+?>
