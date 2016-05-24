e11703824f086f0c96c4101db00cde0416c5000e
h3llyeah <hniilo@gmail.com>
2016-05-24 11:26:48 +0300

    Update yl0305.php

--- ylesanded/yl0305.php
+++ ylesanded/yl0305.php
@@ -1,22 +1,28 @@
 https://github.com/h3llyeah/phpkursus2016/tree/master/ylesanded </br></br>
 <?php
+
 function string_info($i)
 {
-	echo "Sõne algab : ".$i[0]."<br>";
-	echo "Sõne lõppeb : ".$i[(strlen($i)-1)]."<br>";
-	echo "Sõne lowercase : ".strtolower($i)."<br>";
+	echo "Sõne algab :".$i[0]."<br>";
+	echo "Sõne lõppeb :".$i[(strlen($i)-1)]."<br>";
+	echo "Sõne lowercase :".strtolower($i)."<br>";
 	$j=strtolower($i);
-	echo "Sõne uppercase : ".strtoupper($i)."<br>";
+	echo "Sõne uppercase :".strtoupper($i)."<br>";
 	
+    $j=0;
 	for ($k=0; $k<=strlen($i); $k++)
 	{
         if ($i[$k]=="a")
         {
-            $b++;
+		$j++;
+
         }
 	}
-    echo $b;
+    echo $j;
 }
-$string="ABaacdeaA";
+
+$string="ABcadeaA";
+
 string_info($string);
+
 ?>
